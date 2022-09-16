0f90ed8192d6cdb4a8329204cf6d75f195ca1702
dfdiazc <df.diazc@uniandes.edu.co>
2022-09-16 14:58:07 -0500

    Modified frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<main><p>Homepage</p></main>} />
+        <Route path="/*" element={<main><p>Not Found</p></main>} />
+      </Routes>
+  </BrowserRouter>
   );
 }
+
+
 
 export default App;
