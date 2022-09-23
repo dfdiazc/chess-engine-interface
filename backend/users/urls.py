087a3bd544f1b1b252c52aa6c17a0c9e73887a9b
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-09-23 09:03:11 -0500

    Added login, logout and user creation views

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("", views.index),
+    path("create", views.CreateView.as_view()), # Create a user
+    path("login", views.LoginView.as_view()), # Log a user in
+    path("logout", views.LogoutView.as_view()), # Log a user out
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
 from django.http import JsonResponse
+from django.views import View
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
 
 # Create your views here.
-def index(request):
 
-    return JsonResponse({"Users app": "OK"})
+class CreateView(View):
+
+    def post(self, request):
+
+        username = request.POST.get("email") # The username is set as the user's email
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = User.objects.create_user(username, email, password)
+
+        return JsonResponse({"status": "user created successfully"})
+
+class LoginView(View):
+
+    def post(self, request):
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if(user is not None):
+
+            login(request, user)
+
+            return JsonResponse({"status": "logged in"})
+
+        else:
+
+            return JsonResponse({"status": "invalid credentials"})
+
+class LogoutView(View):
+
+    def get(self, request):
+
+        logout(request)
+
+        return JsonResponse({"status": "logged out"})
