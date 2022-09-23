42ef5c4a0fdd9a8e46b60b87748b152bce65455f
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-09-23 11:09:55 -0500

    Changed DEBUG = False

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%jg=@g904rz=)x^pqg1r1vp$ve65!^nl=(@lib8do8vo#uh*q6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['unrealchess.pythonanywhere.com']
 
