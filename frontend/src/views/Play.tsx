5aa1a0090828e447ea324095bf6246746f0b3476
dfdiazc <df.diazc@uniandes.edu.co>
2022-09-24 00:29:57 -0500

    Modified Game Logic

--- frontend/src/views/Play.tsx
+++ frontend/src/views/Play.tsx
@@ -1,58 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Header } from "components";
-import { Chessboard } from "react-chessboard";
+import { Chessboard, Square } from "react-chessboard";
 import { Chess } from "chess.js";
 
 const Play = () => {
   const [game, setGame] = useState(new Chess());
-  const [latestTimeout, setLatestTimeout] = useState();
+  const [fen, setFen] = useState(game.fen());
 
-  function safeGameMutate(modify:any) {
-    setGame((g:any) => {
-      const update = { ...g };
-      modify(update);
-      return update;
-    });
-  }
-
-  useEffect(() => {
-    setTimeout(makeRandomMove, 1000);
-    return () => {
-      clearTimeout(latestTimeout);
-    };
-  }, []);
-
-  function makeRandomMove() {
-    const possibleMoves = game.moves();
-
-    // exit if the game is over
-    if (game.isGameOver() === true || game.isDraw() === true || possibleMoves.length === 0) {
-      return;
+  function onDrop(source: Square, target: Square) {
+    let result = game.move({ from: source, to: target });
+    if (result != null) {
+      setFen(game.fen());
+      return true;
     }
-
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-    safeGameMutate(() => {
-      game.move(possibleMoves[randomIndex]);
-    });
-
-    const timeout:any = setTimeout(makeRandomMove, 300);
-    setLatestTimeout(timeout);
-    console.log(game.fen())
+    return false;
   }
 
   return (
     <div className="h-full">
       <Header />
       <div className="flex gap-5 px-24 py-12">
-      <Chessboard
-        arePiecesDraggable={false}
-        position={game.fen()}
-        animationDuration={200}
-        customBoardStyle={{
-          borderRadius: '4px',
-          boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)'
-        }}
-      />
+        <Chessboard position={fen} onPieceDrop={onDrop} />
       </div>
     </div>
   );
