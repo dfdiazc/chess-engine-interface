97f1654109b9878655890f1ddd0de9a04275bfb5
dfdiazc <df.diazc@uniandes.edu.co>
2022-09-28 15:23:55 -0500

    Added Animated Turn Indicator

--- frontend/src/views/Play.tsx
+++ frontend/src/views/Play.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 const Play = () => {
   const [game] = useState(new Chess());
   const [fen, setFen] = useState(game.fen());
+  const [turn, setTurn] = useState(game.turn());
   interface response {
     response: string | "No Response";
   }
@@ -15,22 +16,47 @@ const Play = () => {
   function onDrop(source: Square, target: Square) {
     let result = game.move({ from: source, to: target });
     if (result != null) {
-      axios.get<response>(`https://unrealchess.pythonanywhere.com/api/mods/${game.fen().replaceAll('/', '-')}$`)
-      .then((response) => {
-        setResponse(response.data)
-      console.log(response.data)
-    })
+      axios
+        .get<response>(
+          `https://unrealchess.pythonanywhere.com/api/mods/${game
+            .fen()
+            .replaceAll("/", "-")}$`
+        )
+        .then((response) => {
+          setResponse(response.data);
+          console.log(response.data);
+        });
       setFen(game.fen());
+      setTurn(game.turn());
+      console.log(turn)
       return true;
     }
     return false;
   }
+  let styleTop = {
+    transform: "translateY(-536px)",
+    backgroundColor: "black",
+    borderWidth: "1px",
+    border: "solid 1px white",
+  };
+  let styleBottom = {
+    transform: "translateY(0%)",
+    backgroundColor: "white",
+    borderWidth: "1px",
+    border: "solid 1px black",
+  };
 
   return (
     <div className="h-full">
       <Header />
-      <div className="flex gap-5 px-24 py-12">
+      <div className="flex px-24 py-12">
         <Chessboard position={fen} onPieceDrop={onDrop} />
+        <div className="flex justify-center w-10 relative">
+          <div
+            className="w-6 h-6 bg-blue-600 rounded-full absolute transition-all bottom-0"
+            style={turn === "w" ? styleBottom : styleTop}
+          ></div>
+        </div>
         <span></span>
       </div>
     </div>
