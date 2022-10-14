de1852a63ecb21c848a6cd7171e9670f8d517017
Daniel Díaz <df.diazc@uniandes.edu.co>
2022-10-13 19:40:37 -0500

    Added Ad Element on Play View

--- frontend/src/views/Play.tsx
+++ frontend/src/views/Play.tsx
@@ -4,15 +4,15 @@ import { Helmet } from "react-helmet";
 import { Adsense } from "@ctrl/react-adsense";
 
 const Play = () => {
-  const [boardWidth, setBoardWidth] = useState(initialSize);
-  function initialSize() {
+  const [boardWidth, setBoardWidth] = useState(initialBoardSize);
+  function initialBoardSize() {
     let windowWidth = window.innerWidth;
-    if (windowWidth < 1024) return windowWidth * 0.8;
+    if (windowWidth < 768) return windowWidth * 0.8;
     else return 600;
   }
   const handleResize = () => {
     let windowWidth = window.innerWidth;
-    if (windowWidth < 1024) setBoardWidth(windowWidth * 0.8);
+    if (windowWidth < 768) setBoardWidth(windowWidth * 0.8);
     else setBoardWidth(600);
   };
   useEffect(() => {
@@ -24,22 +24,23 @@ const Play = () => {
         <div className="w-full max-w-[1920px]">
           <div className="flex flex-col">
             <div className="flex justify-center">
-            <Helmet>
-              <style>{"body {background-color: #121B1E}"}</style>
-            </Helmet>
-            <Header />
+              <Helmet>
+                <style>{"body {background-color: #121B1E}"}</style>
+              </Helmet>
+              <Header />
             </div>
           </div>
-          <div className="flex justify-center items-center h-[calc(100vh-4rem)] shrink-0 relative">
+          <div className="flex flex-col lg:flex-row justify-center items-center h-full shrink-0 relative">
             <div className="px-3 py-10">
               <CustomChessBoard boardWidth={boardWidth} />
             </div>
+            <div className="hidden lg:flex w-[200px]  h-[600px]">
             <Adsense
               client="ca-pub-7640562161899788"
               slot="7259870550"
-              style={{ width: 200, height: 600 }}
               format=""
             />
+            </div>
           </div>
         </div>
       </div>
