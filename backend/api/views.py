610f0c4301cbc37a513e907837abd064a880cd49
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-10-22 19:34:47 -0500

    Added stockfish engine support
    Included the stockfish engine and the api views to allow to play against it

--- backend/api/views.py
+++ backend/api/views.py
@@ -17,3 +17,17 @@ class CreateMatchView(generics.CreateAPIView):
 
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+
+
+class GetStockfishMove(generics.GenericAPIView):
+
+    def get(self, request, FEN):
+
+        raw_FEN = FEN.split(" ")
+        correction = raw_FEN[0].replace("-", "/")
+
+        new_FEN = " ".join([correction] + raw_FEN[1:])
+
+        best_move = mods.get_stockfish_move(new_FEN)
+
+        return Response({"best_move": best_move})
