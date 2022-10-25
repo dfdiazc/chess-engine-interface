f144c62f8c06de9812f36769f54f4dbcc68f9a78
Daniel Díaz <df.diazc@uniandes.edu.co>
2022-10-24 23:47:02 -0500

    Fixed Typos in Authentication Logic

--- frontend/src/redux/features/auth/authSlice.ts
+++ frontend/src/redux/features/auth/authSlice.ts
@@ -12,19 +12,19 @@ const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {},
-    extraReducers: (builder) => {
+    extraReducers: (builder) => (
         builder.addCase(registerUser.pending, (state, action)  => {
             state.loading = true
         }),
         builder.addCase(registerUser.fulfilled, (state, action)  => {
             state.loading = false
-            state.success =  true
+            state.success = true
         }),
         builder.addCase(registerUser.rejected, (state, action)  => {
             state.loading = false
-            state.success =  false
+            state.success = false
         })
-    },
+    ),
 })
 
 export default authSlice.reducer;
