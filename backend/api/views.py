438219382abdbe27c8480afb692c8abf973d28c5
Juan José González Oneto <jj.gonzalez20@uniandes.edu.co>
2022-10-28 19:47:08 -0500

    Added views for stockfish
    Added view to suggest moves and to set the ELO rating of the engine

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,19 @@ from . import mods
 from .models import Match
 from .serializers import MatchSerializer
 
-# Create your views here.
+# Auxiliary functions
+
+def fix_fen(url_fen):
+
+    raw_FEN = url_fen.split(" ")
+    correction = raw_FEN[0].replace("-", "/")
+
+    new_FEN = " ".join([correction] + raw_FEN[1:])
+
+    return new_FEN
+
+
+# Views
 class LostView(generics.GenericAPIView):
 
     def get(self, request, FEN):
@@ -23,11 +35,34 @@ class GetStockfishMove(generics.GenericAPIView):
 
     def get(self, request, FEN):
 
-        raw_FEN = FEN.split(" ")
-        correction = raw_FEN[0].replace("-", "/")
-
-        new_FEN = " ".join([correction] + raw_FEN[1:])
+        new_FEN = fix_fen(FEN)
 
         best_move = mods.get_stockfish_move(new_FEN)
 
         return Response({"best_move": best_move})
+
+class GetStockfishBestMoves(generics.GenericAPIView):
+
+    def get(self, request, FEN):
+
+        new_FEN = fix_fen(FEN)
+
+        moves = mods.get_stockfish_nbest_moves(new_FEN)
+
+        dic = {}
+
+        for i in range(len(moves)):
+
+            dic[str(i+1)] = str(moves[i])
+
+        return Response(dic)
+
+class GetStockfishMoveELO(generics.GenericAPIView):
+
+    def get(self, request, ELO, FEN):
+
+        new_FEN = fix_fen(FEN)
+
+        best_move_elo = mods.get_stockfish_move_elo(ELO, new_FEN)
+
+        return Response({"best_move_elo": best_move_elo})
