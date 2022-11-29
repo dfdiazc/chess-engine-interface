71b6876429eabfd3cd44680ea39ad973359b666a
dfdiazc <df.diazc@uniandes.edu.co>
2022-11-29 11:03:15 -0500

    Modified Promotion Piece Selector

--- frontend/src/components/PromotionPieceSelector.tsx
+++ frontend/src/components/PromotionPieceSelector.tsx
@@ -1,35 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentPlayerColor } from "features/chess/chessSlice";
+import { setPromoPiece } from "features/chess/chessSlice";
 
 const PromotionPieceSelector = () => {
-  const pieces = [
-    "wP",
-    "wN",
-    "wB",
-    "wR",
-    "wQ",
-    "wK",
-    "bP",
-    "bN",
-    "bB",
-    "bR",
-    "bQ",
-    "bK",
-  ];
+  const dispatch = useDispatch();
+  const playerColor = useSelector(selectCurrentPlayerColor);
+  const pieces = ["N", "B", "R", "Q"];
   return (
-    <div className="w-fit absolute top-10 left-0 right-0 mx-auto block bg-[#3D4547] p-5 z-10 rounded-lg">
-      <>
-        {pieces.map((p) => {
-          <div
-            className="bg-center bg-no-repeat"
-            style={{
-              width: "10rem",
-              height: "10rem",
-              backgroundImage: `url(http://146.190.33.159/static/chess/pieces/pirouetti/${p}.svg)`,
-              backgroundSize: "100%",
-            }}
-          />;
-        })}
-      </>
+    <div className="w-full absolute flex justify-center top-0 left-0 right-0 mx-auto p-5 z-10">
+      <div className="flex gap-3 w-fit bg-[#3D4547] rounded-lg px-20 py-5">
+      {pieces.map((piece, i) => (
+        <button
+          className="bg-center bg-no-repeat hover:bg-flamingo-100 rounded-lg"
+          key={i}
+          onClick={() => {dispatch(setPromoPiece(piece.toLowerCase()))}}
+          style={{
+            width: "5rem",
+            height: "5rem",
+            backgroundImage: `url(http://146.190.33.159/static/chess/pieces/staunty/${
+              playerColor + piece
+            }.svg)`,
+            backgroundSize: "100%",
+          }}
+        />
+      ))}
+      </div>
     </div>
   );
 };
